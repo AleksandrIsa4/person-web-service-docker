d64026bed317d2d08af0d812dd4b03346ae74a74
AleksandrIsa4 <isachenkov.am@yandex.ru>
2023-11-12 16:52:13 +0300

    Правка deploy-job.yml в11

--- person-gateway/src/main/java/com/person/web/controller/PersonController.java
+++ person-gateway/src/main/java/com/person/web/controller/PersonController.java
@@ -29,9 +29,6 @@ public class PersonController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getPeople(@PathVariable("id") Long personId) {
-        if(personId==2){
-            System.out.println("Hello World!999");
-        }
         return ResponseEntity.accepted().body("Hello World!999");
      //   return personClient.getPerson(personId);
     }
